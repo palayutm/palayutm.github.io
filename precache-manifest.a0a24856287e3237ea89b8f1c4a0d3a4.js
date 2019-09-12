b1eba07c79263d5ac44a258a67c5d1e1c37a5292
palayutm <palayutm@gmail.com>
2019-09-12 22:31:32 +0800

    add contests list

---
+++ precache-manifest.a0a24856287e3237ea89b8f1c4a0d3a4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bf0dc108ce8aa271c996615ca76ea57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d637372a4cf75b43d490",
+    "url": "/static/js/2.bf666ea7.chunk.js"
+  },
+  {
+    "revision": "ad742466d03be5e0e208",
+    "url": "/static/js/main.9c1eb0d4.chunk.js"
+  },
+  {
+    "revision": "3380e4f8b497c9ea097f",
+    "url": "/static/js/runtime~main.78240119.js"
+  }
+]);
