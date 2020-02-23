4b70c0d7df6d9e30276ae46eed1b202c9ace240a
palayutm <palayutm@gmail.com>
2020-02-24 00:11:55 +0800

    auto commit

---
+++ precache-manifest.becc99a115d107f94c657e536e284095.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68d165dc5e7fbbd7e14bc267000717bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70ad92566e2f72c88d11",
+    "url": "/static/js/2.900ec9a1.chunk.js"
+  },
+  {
+    "revision": "b832cdd8f9b38ee270a6",
+    "url": "/static/js/main.02a5cd8b.chunk.js"
+  },
+  {
+    "revision": "3380e4f8b497c9ea097f",
+    "url": "/static/js/runtime~main.78240119.js"
+  }
+]);
